c3d5b71b32f3eb5dac9e5f5035d214debbc39f74
Carlos Varela <cvarela@legaldecoder.com>
2022-05-30 18:43:27 -0600

    Add project files.

---
+++ InsuranceCompanySystem.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using InsuranceCompanySystem.App;
+
+Console.WriteLine("Insurance System Starting...");
+
+var engine = new InsuranceEngine();
+engine.Rate();
+
+if (engine.Rating > 0)
+{
+    Console.WriteLine($"Rating: {engine.Rating}");
+}
+else
+{
+    Console.WriteLine("No rating produced.");
+}
